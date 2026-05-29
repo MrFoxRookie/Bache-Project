cad05befe3003a199969886a2d6c9881e526a2e3
MrFoxRookie <Zahidtejedaamaro@gmail.com>
2026-05-29 17:37:30 -0600

    feat: add map and user location detection button

--- frontend/src/components/Main/Main.tsx
+++ frontend/src/components/Main/Main.tsx
@@ -1,6 +1,15 @@
+// import styles from "./Main.module.css"
+import {useEffect} from "react"
+import Map from "../Map/Map"
 function Main() {
+
+  useEffect(() => {
+    
+  }, [])
  
-  return <></>
+  return <>
+  <Map/>
+  </>
 }
 
 export default Main                                            
---
+++ frontend/src/services/map.js
@@ -0,0 +1,5 @@
+function handleResponse() {}
+
+function mapApi() {
+  fetch();
+}
